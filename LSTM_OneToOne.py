5d4d7db18ae898127ce20be9ab541f3aed3f906f
AxekA13 <39571553+AxekA13@users.noreply.github.com>
2020-02-06 03:22:26 +0300

    Removed unnecessary code

--- LSTM_OneToOne.py
+++ LSTM_OneToOne.py
@@ -159,59 +159,6 @@ def create_dataset(dataset, look_back=1):
     return np.array(dataX), np.array(dataY)
 
 
-"""
-
-# При look_back = 1 предсказывает значение по предыдущему
-look_back = 5000
-
-trainX, trainY = create_dataset(train_data, look_back)
-testX, testY = create_dataset(test_data, look_back)
-# Преобразование пайплайна из 2D в 3D для подачи LSTM
-print(trainX.shape)
-print(trainY.shape)
-trainX = np.reshape(trainX, (trainX.shape[0], 1, trainX.shape[1]))
-testX = np.reshape(testX, (testX.shape[0], 1, testX.shape[1]))
-
-# Создание модели 1
-
-
-model = Sequential()
-model.add(LSTM(50, return_sequences=True, input_shape=(trainX.shape[1], trainX.shape[2]), activation='linear'))
-model.add(Dropout(0.3))
-model.add(LSTM(20, return_sequences=False))
-model.add(Dropout(0.3))
-model.add(Dense(1, activation='linear'))
-model.compile(loss='mse', optimizer='adam')
-history = model.fit(trainX, trainY, batch_size=100, epochs=10, validation_data=(testX, testY))
-
-# График тренировочной и валидационной выборки
-
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='test')
-plt.legend()
-plt.show()
-
-# Предсказываем значение и сравниваем его, построив график
-yhat = model.predict(testX)
-
-# Преобразовываем данные в исходный формат для проверки RMSE
-
-yhat_inverse = scaler.inverse_transform(yhat.reshape(-1, 1))
-testY_inverse = scaler.inverse_transform(testY.reshape(-1, 1))
-
-plt.plot(yhat_inverse[0:300], label='predict')
-plt.plot(testY_inverse[0:300], label='true')
-plt.legend()
-plt.show()
-
-# RMSE
-rmse = sqrt(mean_squared_error(testY_inverse, yhat_inverse))
-print('Test RMSE: %.3f' % rmse)
-
-
-# Предсказываем значение для даты, которую модель не видела
-"""
-
 
 # Предсказывание нескольких значений
 def split_sequence(sequence, n_steps_in, n_steps_out):
@@ -237,7 +184,7 @@ d['Datetime'] = pd.to_datetime(d['Datetime'])
 d.sort_values('Datetime')
 d = d.drop(['Datetime'], axis=1)
 d = np.array(d)
-cutoff_point = int(len(d) * 0.8)
+cutoff_point = int(len(d) * 0.7)
 train_data = d[:cutoff_point]
 test_data = d[cutoff_point + 1:]
 train_data = scaler.fit_transform(train_data).ravel()
@@ -245,7 +192,7 @@ test_data = scaler.transform(test_data).ravel()
 train_data = list(train_data.ravel())[:]
 test_data = list(test_data.ravel())[:]
 
-lookback = 30  # на сколько шагов назад смотрим при предсказании
+lookback = 10  # на сколько шагов назад смотрим при предсказании
 feature = 1  # входной вектор
 count_of_predict = 1  # сколько предсказываем
 xTrain, yTrain = split_sequence(train_data, lookback, count_of_predict)
@@ -255,9 +202,9 @@ xTest = xTest.reshape((xTest.shape[0], xTest.shape[1], feature))
 # Модель 2
 model = Sequential()
 model.add(
-    LSTM(50, input_shape=(lookback, feature), activation='linear'))
-# model.add(Dropout(0.1))
-# model.add(LSTM(50))
+    LSTM(50, return_sequences=True, input_shape=(lookback, feature), activation='linear'))
+#model.add(Dropout(0.1))
+model.add(LSTM(50, activation='linear'))
 # model.add(Dropout(0.1))
 model.add(Dense(count_of_predict, activation='linear'))
 model.compile(optimizer='adam', loss='mse', )
@@ -266,26 +213,40 @@ yhat = model.predict(xTest, verbose=0)
 yTest = scaler.inverse_transform(yTest)
 yhat = scaler.inverse_transform(yhat)
 
-# Все предсказания и настоящие значение
+title = 'LSTM_1to1 2 layers, 50 neurons. 10 epochs.Act=linear, look=10,pred=1'
+plt.plot(history.history['loss'], label='train')
+plt.plot(history.history['val_loss'], label='test')
+plt.legend()
+plt.title(title)
+plt.show()
+
+# Проверка RMSE
+rmse = sqrt(mean_squared_error(yTest, yhat))
+rmse_show = 'Test RMSE: %.3f' % rmse
+print('Test RMSE: %.3f' % rmse)
+plt.xlabel(rmse_show)
+
+# Все предсказания и настоящее значение
 for i in range(count_of_predict):
-    plt.plot(yhat[0:300, i], label='predict' + str(i + 1))
+    plt.plot(yhat[0:300, i], label='predict ' + str(i + 1))
 plt.plot(yTest[0:300, 0], label='true')
 plt.legend()
+plt.xlabel(rmse_show)
+plt.title(title)
 plt.show()
 
 # Проверить сдвиги во времени
 plt.plot(yhat[0:20, 0], label='predict' + str(1))
 plt.plot(yhat[0:20, 1], label='predict' + str(2))
 plt.legend()
+plt.xlabel(rmse_show)
+plt.title(title)
 plt.show()
-
 # Вывод по очереди всех предсказанных значений и их настоящих
 for i in range(count_of_predict):
-    plt.plot(yhat[0:300, i], label='predict' + str(i + 1))
+    plt.plot(yhat[0:300, i], label='predict ' + str(i + 1))
     plt.plot(yTest[0:300, i], label='true')
     plt.legend()
+    plt.title(title)
+    plt.xlabel(rmse_show)
     plt.show()
-
-# Проверка RMSE
-rmse = sqrt(mean_squared_error(yTest, yhat))
-print('Test RMSE: %.3f' % rmse)
